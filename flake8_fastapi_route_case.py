80e3bb005f638b939aac93a05d0de4142826dd34
Harry Lees <harry.lees@gmail.com>
2023-01-25 14:45:18 +0000

    added support for snake_case and lowerCamelCase string validation

--- flake8_fastapi_route_case.py
+++ flake8_fastapi_route_case.py
@@ -1,6 +1,9 @@
+import argparse
 import ast
 import re
 import sys
+from enum import Enum
+from pathlib import Path
 from typing import Any
 from typing import Generator
 from typing import Tuple
@@ -13,15 +16,35 @@ else:
     import importlib.metadata as importlib_metadata
 
 
-class Visitor(ast.NodeVisitor):
-    def __init__(self) -> None:
-        self.errors: list[tuple[int, int, str]] = []
+class SupportedCase(Enum):
+    SNAKE = "snake"
+    CAMEL = "camel"
 
-    def is_snake_case(self, s: str) -> bool:
-        SNAKE_CASE_TEST_RE = re.compile(r"^[a-z][a-z0-9]+(_[a-z0-9]+)*$")
-        if s.startswith("/"):
-            s = s.lstrip("/")
-        return all(SNAKE_CASE_TEST_RE.match(substr) for substr in s.split("/"))
+
+patterns = {
+    # snake_case regex
+    SupportedCase.SNAKE: re.compile(r"^[a-z][a-z0-9]+(_[a-z0-9]+)*$"),
+    # lowerCamelCase regex
+    SupportedCase.CAMEL: re.compile(r"[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?"),
+}
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self, route_case: SupportedCase) -> None:
+        self.errors: list[tuple[int, int, str]] = []
+        self.route_case = route_case
+
+    def match_case(self, path: str, pattern: re.Pattern) -> bool:
+        if path.startswith("/"):
+            path = path.lstrip("/")
+        for subpath in path.split("/"):
+            # Skip route params e.g. /users/{user_id}
+            if subpath.startswith("{") and subpath.endswith("}"):
+                continue
+
+            if not pattern.fullmatch(subpath):
+                return False
+        return True
 
     def generic_visit(self, node: ast.AST) -> None:
         for node in ast.walk(node):
@@ -35,16 +58,23 @@ class Visitor(ast.NodeVisitor):
                 and isinstance(decorator.func, ast.Attribute)
                 and decorator.func.attr in ("get", "post", "put", "delete")
             ):
-                path = decorator.args[0]
-                if not self.is_snake_case(path.value):
+                route = decorator.args[0]
+                if isinstance(route, ast.Constant) and not self.match_case(
+                    route.value, patterns[self.route_case]
+                ):
                     self.errors.append(
-                        (path.lineno, path.col_offset, "Value not snake case")
+                        (
+                            route.lineno,
+                            route.col_offset,
+                            f"Value not {self.route_case.value} case",
+                        )
                     )
 
 
 class Plugin:
     name = "flake8_fastapi_route_case"
     version = importlib_metadata.version(__name__)
+    route_case = SupportedCase.SNAKE
 
     def __init__(self, tree: ast.AST) -> None:
         self._tree = tree
@@ -52,18 +82,26 @@ class Plugin:
     @staticmethod
     def add_options(option_manager: Any) -> None:
         option_manager.add_option(
-            "--case",
-            type=str,
+            "--route-case",
+            type=SupportedCase,
             default="snake",
             parse_from_config=True,
-            metavar="CASE",
+            metavar="route_case",
             help=(
-                "Case styling that routes should conform to, " "(default: %(default)s)"
+                "Case styling that routes should conform to, "
+                "supported options: {} (default: {})".format(
+                    ", ".join(case.value for case in list(SupportedCase)),
+                    SupportedCase.SNAKE.value,
+                )
             ),
         )
 
+    @classmethod
+    def parse_options(cls, options: argparse.Namespace) -> None:
+        cls.route_case = options.route_case
+
     def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
-        visitor = Visitor()
+        visitor = Visitor(self.route_case)
         visitor.visit(self._tree)
 
         for line, col, msg in visitor.errors:
